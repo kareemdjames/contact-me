d47ae390969a57f0d3a9cc2b7cbba23cb00b61e1
Kareem James <kareemjames@Kareems-MacBook-Pro.local>
2022-03-24 11:48:34 -0400

    add logic for adding files to storage, added lorem.txt file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyrebase
 
-firebaseConfig = {
+firebase_config = {
     "apiKey": "AIzaSyAxRQPXM4KwXTaXvG55NZD1ZiAv3kSejrw",
     "authDomain": "contact-me-dcec6.firebaseapp.com",
     "projectId": "contact-me-dcec6",
@@ -12,29 +12,37 @@ firebaseConfig = {
 
 }
 
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebase_config)
 
-auth = firebase.auth()
-# db=firebase.database()
-# storage=firebase.storage()
+#auth = firebase.auth()
+#db=firebase.database()
+storage=firebase.storage()
 
 # Authentication
 # Login
-email = input("Enter your email")
-password = input("Enter your password")
-try:
-    auth.sign_in_with_email_and_password(email, password)
-    print("Successfully signed in")
-except:
-    print("Invalid email, or password. Please try again")
+# email = input("Enter your email: ")
+# password = input("Enter your password: ")
+# try:
+#     auth.sign_in_with_email_and_password(email, password)
+#     print("Successfully signed in")
+# except:
+#     print("Invalid email, or password. Please try again")
 
 # Signup
-email = input("Enter your email")
-password = input("Enter your password")
-confirm_password = input("Please confirm your password")
-if password == confirm_password:
-    try:
-        auth.create_user_with_email_and_password(email, password)
-        print("Success!")
-    except:
-        print('Email already exists, please login with your password')
+# email = input("Enter your email: ")
+# password = input("Enter your password: ")
+# confirm_password = input("Please confirm your password: ")
+# if password == confirm_password:
+#     try:
+#         auth.create_user_with_email_and_password(email, password)
+#         print("Success!")
+#     except:
+#         print('Email already exists, please login with your password')
+
+# Storage
+filename = input("Enter the name of the file you want to upload: ")
+# You can save the file to a path you specify e.g: \books\poem\cloud_filename. If the path doesn't exsist it will be created.
+cloud_filename = input("Enter the name you want the file in the cloud: ")
+storage.child(cloud_filename).put(filename)
+# Prints the url of the uploaded file
+print(storage.child(cloud_filename).get_url(None))
