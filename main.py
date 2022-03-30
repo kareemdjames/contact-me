9f3720ddd9a65ab950032eb36b574461dca7e7df
Kareem James <kareemdjames@gmail.com>
2022-03-30 14:23:48 -0400

    refactored some code, removed most of the unused code

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import urllib.request
-
 import pyrebase
 from dotenv import load_dotenv
 import os
@@ -21,22 +19,12 @@ firebase_config = {
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
-
 auth = firebase.auth()
 db = firebase.database()
 
 
-# storage=firebase.storage()
-
 # Authentication
 # Login
-# email = input("Enter your email: ")
-# password = input("Enter your password: ")
-# try:
-#     auth.sign_in_with_email_and_password(email, password)
-#     print("Successfully signed in")
-# except:
-#     print("Invalid email, or password. Please try again")
 def login(email, password):
     try:
         auth.sign_in_with_email_and_password(email, password)
@@ -56,49 +44,12 @@ def login(email, password):
 #     except:
 #         print('Email already exists, please login with your password')
 
-# # Storage
-# # Upload
-# filename = input("Enter the name of the file you want to upload: ")
-# # You can save the file to a path you specify e.g: \books\poem\cloud_filename. If the path doesn't exsist it will be created.
-# cloud_filename = input("Enter the name you want the file in the cloud: ")
-# storage.child(cloud_filename).put(filename)
-# # Prints the url of the uploaded file
-# print(storage.child(cloud_filename).get_url(None))
-
-# # Download
-# cloud_filename = input("Enter the name of the file you want to download")
-# storage.child(cloud_filename).download("", "downloaded.txt")
-#
-# # Reading file to command line
-# cloud_filename = input("Enter the name of the file you want to download")
-# url = storage.child(cloud_filename).get_url(None)
-# file = urllib.request.urlopen(url).read()
-# print(file)
-
-# Database
 # Create
-# db = root, db.child = child under root
-# data = {"first_name": "Karlos", "last_name": "Tempes", "age": 21}
-# db.child("contacts").push(data)
-# Use custom id vs firebase generated one
-# db.child("contacts").child("user_id").set(data)
 def create_contact(data):
     db.child("contacts").push(data)
 
 
-# # Update
-# # Use this is you already know the key
-# #db.child("contacts").child("user_id").update({"last_name":"James"})
-#
-# # Get all contacts
-# contacts = db.child("contacts").get()
-# # Iterate to get values from contacts including userid
-# for contact in contacts.each():
-#     #print(contact.val())
-#     #print(contact.key())
-#     if contact.val()['name'] == 'Mark':
-#         db.child("contacts").child(contact.key)
-
+# Update
 def update_contact(name):
     is_contact_updated = False
     contacts = db.child("contacts").get()
@@ -112,16 +63,7 @@ def update_contact(name):
             is_contact_updated = True
 
 
-# # Delete
-# # If you know the id
-# db.child("contacts").child("contact").remove()
-# # Specific field
-# db.child("contacts").child("contact").child("first_name").remove()
-# # If you don't know the id
-# contacts = db.child("contacts").get()
-# for contact in contacts.each():
-#     if contact.val()['first_name'] == 'Ben':
-#         db.child("contacts").child(contact.key()).child("age").remove()
+# Delete
 def delete_contact(name):
     is_contact_deleted = False
     contacts = db.child("contacts").get()
@@ -134,28 +76,21 @@ def delete_contact(name):
         print("No contact with this name found")
 
 
-# #Read
-# # Get everything
-# #contacts = db.child("contacts").get()
-# #Get single
-# contacts = db.child("contacts").order_by_child("first_name").equal_to("Anna").get()
-# for contact in contacts.each():
-#     print(contact.val()["age"])
-#
-# contacts = db.child("contacts").order_by_child("age").start_at(20).get()
-# for contact in contacts.each():
-#     print(contact.val())
+# Get all contacts
 def get_all_contacts():
     contacts = db.child("contacts").get()
     for contact in contacts.each():
         print(contact.val())
 
 
+# Get single contact
 def find_contact(name):
     contacts = db.child("contacts").order_by_child("first_name").equal_to(name).get()
     for contact in contacts.each():
         print(contact.val())
 
+
+# Program start
 print("Welcome to the address book program")
 
 users_input = "".lower()
@@ -211,3 +146,4 @@ while users_input != "q":
         break
 
 print("Thank you for using the address book")
+# Program End
